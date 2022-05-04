777ea7ff6960d26a679d4e55a81cca5582e25807
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-05-04 11:34:22 -0400

    added the detail view of the nft

--- src/components/NFTCard.js
+++ src/components/NFTCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,24 +7,47 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Dialog from '@material-ui/core/Dialog';
+import CloseIcon from '@material-ui/icons/Close';
+import Grid from '@material-ui/core/Grid';
+import theme from 'theme';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: 545,
   },
   media: {
     height: 140,
     marginTop: 16,
     backgroundSize: 'contain'
   },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    borderRadius: 15,
+    width: 500
+  },
+  padding: {
+    padding: theme.spacing(2, 4, 3),
+  },
+  close: {
+    cursor: 'pointer',
+    color: 'gray'
+  },
 });
 
 export default function NFTCard({ name, image, description, attributes, tokenId }) {
   const classes = useStyles();
 
+  const [open, setOpen] = useState(false);
   const handleClickDetail = () => {
-
+    setOpen(true);
   }
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Card className={classes.root}>
       <CardActionArea>
@@ -43,12 +66,27 @@ export default function NFTCard({ name, image, description, attributes, tokenId 
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary"  onClick={handleClickDetail}>
+        {/* <Button size="small" color="primary"  onClick={handleClickDetail}>
           Share
-        </Button>
+        </Button> */}
         <Button size="small" color="primary" onClick={handleClickDetail}>
           View Details
         </Button>
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+            classes={{ paper: classes.paper }}
+        >
+            <Grid container className={classes.padding} justify="space-between">
+                <Grid item ><p className={classes.modalTitle}><b>{name}</b></p></Grid>
+                <Grid item style={{display: 'flex'}}><CloseIcon onClick={handleClose} className={classes.close} /></Grid>
+            </Grid>
+            <Scrollbars style={{ height: '50vh' }}>
+                <img src={image} width="100%"/>
+            </Scrollbars>
+        </Dialog>
       </CardActions>
     </Card>
   );
